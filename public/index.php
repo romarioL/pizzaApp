271d4ef0e7ca78d359a8436b5c9182d7312ee48f
Romis Sousa <estreladamanha585@gmail.com>
2020-08-04 20:26:41 -0300

    index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use App\Config\Connection;
-use App\Entities\Product;
+use App\Entities\Produto;
 
 $paths = [__DIR__."/../app/Entities"];
 $isDevMode = true;
@@ -11,14 +11,14 @@ $params = array(
     'driver'   => 'pdo_mysql',
     'user'     => 'root',
     'password' => '',
-    'dbname'   => 'teste',
+    'dbname'   => 'pizzaApp',
 );
 
 $connection = new Connection();
 
 $con = $connection->connect($paths, $isDevMode, $params);
 
-$produto = new Product;
+$produto = new Produto;
 
 $produto->setNome('Bicicleta');
 $produto->setPreco(800);
@@ -26,3 +26,8 @@ $produto->setDescricao('Engrenagem fixa, azul, rápida');
 
 $con->persist($produto);
 $con->flush();
+
+$productCount = $con->getRepository(Produto::class)
+                         ->count(['nome' => 'Bicicleta']);
+
+echo $productCount;
